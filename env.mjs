a6e710efe5ddfa6886ff917361dd39ffdcb76286
Owen Prosser <owenprosser@icloud.com>
2023-06-10 10:22:22 +0100

    Setup next-auth email

--- env.mjs
+++ env.mjs
@@ -4,5 +4,12 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string(),
+    NEXTAUTH_URL: z.string().url().optional(),
+    NEXTAUTH_SECRET: z.string().min(1),
+    EMAIL_SERVER_USER: z.string().min(1),
+    EMAIL_SERVER_PASSWORD: z.string().min(1),
+    EMAIL_SERVER_HOST: z.string().min(1),
+    EMAIL_SERVER_PORT: z.string().min(1),
+    EMAIL_FROM: z.string().min(1),
   },
 });
