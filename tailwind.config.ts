584a10ef0f57d76417b32d1d3ad4e9a82911e99a
Owen Prosser <owenprosser@icloud.com>
2023-06-10 20:56:56 +0100

    Configure tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,34 @@
 import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./app/**/*.tsx"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
       fontFamily: {
         sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
       },
+      borderColor: ({ theme }) => ({
+        DEFAULT: theme("colors.border"),
+      }),
+      colors: {
+        base: {
+          background: "hsl(var(--background) / <alpha-value>)",
+          foreground: "hsl(var(--foreground) / <alpha-value>)",
+        },
+        muted: {
+          background: "hsl(var(--muted-background) / <alpha-value>)",
+          foreground: "hsl(var(--muted-foreground) / <alpha-value>)",
+        },
+        primary: {
+          accent: "hsl(var(--primary-accent) / <alpha-value>)",
+          DEFAULT: "hsl(var(--primary) / <alpha-value>)",
+        },
+        border: "hsl(var(--border) / <alpha-value>)",
+        ring: "hsl(var(--ring) / <alpha-value>)",
+      },
     },
   },
-  plugins: [],
+  plugins: [tailwindcssAnimate],
 } satisfies Config;
