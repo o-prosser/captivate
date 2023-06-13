01cfaa3d7d7a11f598d722b13bd4383d18a174f8
Owen Prosser <owenprosser@icloud.com>
2023-06-13 09:48:48 +0100

    Add email to check user auth

--- app/(routes)/(app)/dashboard/page.tsx
+++ app/(routes)/(app)/dashboard/page.tsx
@@ -1,9 +1,12 @@
 import { Heading } from "@/ui";
+import { getCurrentUser } from "@/util/session";
 
-const Dashboard = () => {
+const Dashboard = async () => {
+  const user = await getCurrentUser();
+
   return (
     <main>
-      <Heading>Welcome back {`{name}`}</Heading>
+      <Heading>Welcome back {user?.name || user?.email}</Heading>
     </main>
   );
 };
