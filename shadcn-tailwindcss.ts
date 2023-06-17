bd5b7d7ee9965a2c45c542de9e9213fcc9f08dc8
Owen Prosser <owenprosser@icloud.com>
2023-06-17 09:52:36 +0100

    Extract shadcn variables to plugin

---
+++ shadcn-tailwindcss.ts
@@ -0,0 +1,82 @@
+import plugin from "tailwindcss/plugin";
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+const shadcnPlugin = plugin(
+  ({ addBase }) => {
+    addBase({
+      ":root": {
+        "--background": "0 0% 255%", // white
+        "--foreground": "229 84% 5%", // slate-950
+        "--muted": "210 40% 96%", // slate-100
+        "--muted-foreground": "215 16% 47%", // slate-500
+        "--primary": "226 56% 35%", // primary-7
+        "--primary-foreground": "210 40% 98%", // slate-50
+        "--destructive": "8 71% 50%", // red
+        "--destructive-foreground": "210 40% 98%", // slate-50
+        "--border": "214 32% 91%", // slate-200
+        "--ring": "var(--primary)",
+      },
+      ".dark": {
+        "--background": "229 84% 5%", // slate-950
+        "--foreground": "0 0% 255%", // white
+        "--muted": "219 33% 17%", // slate-800
+        "--muted-foreground": "215 16% 47%", // slate-500
+        "--primary": "226 56% 35%", // primary-7
+        "--primary-foreground": "210 40% 98%", // slate-50
+        "--destructive": "8 71% 50%", // red
+        "--destructive-foreground": "229 84% 5%", // slate-950
+        "--border": "217 33% 17%", // slate-800
+        "--ring": "var(--primary)",
+      },
+    });
+  },
+  {
+    theme: {
+      fontFamily: {
+        sans: [
+          ["var(--font-sans)", ...fontFamily.sans],
+          {
+            fontFeatureSettings: "'ss01' on",
+          },
+        ],
+      },
+      borderColor: ({ theme }) => ({
+        DEFAULT: theme("colors.border"),
+      }),
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        ring: "hsl(var(--ring))",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+);
+
+export default shadcnPlugin;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,58 +1,9 @@
 import type { Config } from "tailwindcss";
-import defaultTheme from "tailwindcss/defaultTheme";
 import tailwindcssAnimate from "tailwindcss-animate";
+import shadcnPlugin from "./shadcn-tailwindcss";
 
 export default {
   content: ["./app/**/*.tsx"],
   darkMode: "class",
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
-          {
-            fontFeatureSettings: "'ss01' on",
-          },
-        ],
-      },
-      borderColor: ({ theme }) => ({
-        DEFAULT: theme("colors.border"),
-      }),
-      colors: {
-        background: "hsl(var(--background) / <alpha-value>)",
-        "background-secondary":
-          "hsl(var(--background-secondary) / <alpha-value>)",
-        foreground: "hsl(var(--foreground) / <alpha-value>)",
-        muted: {
-          DEFAULT: "hsl(var(--muted) / <alpha-value>)",
-          foreground: "hsl(var(--muted-foreground) / <alpha-value>)",
-        },
-        primary: {
-          DEFAULT: "hsl(var(--primary) / <alpha-value>)",
-          foreground: "hsl(var(--primary-foreground) / <alpha-value>)",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
-          foreground: "hsl(var(--destructive-foreground) / <alpha-value>)",
-        },
-        border: "hsl(var(--border) / <alpha-value>)",
-        ring: "hsl(var(--ring) / <alpha-value>)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
-  },
-  plugins: [tailwindcssAnimate],
+  plugins: [tailwindcssAnimate, shadcnPlugin],
 } satisfies Config;
