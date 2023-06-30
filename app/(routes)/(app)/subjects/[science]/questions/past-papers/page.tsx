ac220526c69bdd106ca55adddb1bf270d9430423
Owen Prosser <owenprosser@icloud.com>
2023-06-30 14:31:13 +0100

    Add Chemistry papers

--- app/(routes)/(app)/subjects/[science]/questions/past-papers/page.tsx
+++ app/(routes)/(app)/subjects/[science]/questions/past-papers/page.tsx
@@ -5,36 +5,59 @@ import { notFound } from "next/navigation";
 import { columns } from "./columns";
 import { Fragment } from "react";
 
+const physicsTables = [
+  {
+    id: "current",
+    spec: "Current specification (2016–)",
+    units: [
+      "AS Unit 1 — Motion, Energy and Matter",
+      "AS Unit 2 — Electricity and Light",
+    ],
+  },
+  {
+    id: "2009",
+    spec: "Legacy specification (2009–2016)",
+    units: [
+      "AS Unit 1 — Motion, Energy and Charge",
+      "AS Unit 2 — Waves and Particles",
+    ],
+  },
+  {
+    id: "2003",
+    spec: "Legacy specification (2003–2009)",
+    units: [
+      "AS Unit 1 — Waves, Light and Basics",
+      "AS Unit 2 — Quanta and Electricity",
+    ],
+  },
+];
+
+const chemistryTables = [
+  {
+    id: "current",
+    spec: "Current specification (2016–)",
+    units: [
+      "AS Unit 1 — The Language of Chemistry, Structure of Matter and Simple Reactions",
+      "AS Unit 2 — Energy, Rate and Chemistry of Carbon Compounds",
+    ],
+  },
+  {
+    id: "2009",
+    spec: "Legacy specification (2009–2016)",
+    units: ["AS Unit 1", "AS Unit 2"],
+  },
+  {
+    id: "2003",
+    spec: "Legacy specification (2003–2009)",
+    units: ["AS Unit 1", "AS Unit 2", "AS Unit 3a"],
+  },
+];
+
 const PastPapers = async ({ params }: { params: { science: string } }) => {
   const science = getScience(params.science);
   if (!science) notFound();
 
-  const tables = [
-    {
-      id: "current",
-      spec: "Current specification (2016–)",
-      units: [
-        "AS Unit 1 — Motion, Energy and Matter",
-        "AS Unit 2 — Electricity and Light",
-      ],
-    },
-    {
-      id: "2009",
-      spec: "Legacy specification (2009–2016)",
-      units: [
-        "AS Unit 1 — Motion, Energy and Charge",
-        "AS Unit 2 — Waves and Particles",
-      ],
-    },
-    {
-      id: "2003",
-      spec: "Legacy specification (2003–2009)",
-      units: [
-        "AS Unit 1 — Waves, Light and Basics",
-        "AS Unit 2 — Quanta and Electricity",
-      ],
-    },
-  ];
+  const tables = params.science == "physics" ? physicsTables : chemistryTables;
 
   const pastPapers = science.pastPapers.map((paper) => ({
     science: params.science,
