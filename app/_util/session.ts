aa115d96733e67a093df0b17ba8b9a57838c0660
Owen Prosser <owenprosser@icloud.com>
2023-07-19 12:21:45 +0100

    cache user fetch

--- app/_util/session.ts
+++ app/_util/session.ts
@@ -1,20 +1,28 @@
-import { authOptions } from "@/lib/auth";
+import { cache } from "react";
 import { getServerSession } from "next-auth";
+
+import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
+
+const getUser = cache(async (email: string) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return user;
+});
 
 const getCurrentUser = async () => {
   const session = await getServerSession(authOptions);
 
   if (!session?.user?.email) return null;
 
-  const dbUser = await prisma.user.findUnique({
-    where: {
-      email: session.user.email,
-    },
-    select: {
-      id: true,
-    },
-  });
+  const dbUser = await getUser(session.user.email);
 
   return { id: dbUser?.id, ...session?.user };
 };
