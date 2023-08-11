3bc50d0bd0b760facfdd4af6a94964a2fa03c191
Owen Prosser <owenprosser@icloud.com>
2023-08-11 21:25:49 +0100

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,6 @@ export default {
   driver: "pg",
   out: "./drizzle/migrations",
   dbCredentials: {
-    connectionString: `postgres://o-prosser:rYnM27vXQEfi@ep-polished-violet-43602101-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require`,
+    connectionString: `${process.env.DATABASE_URL}?sslmode=require`,
   },
 } satisfies Config;
