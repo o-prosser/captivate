140e7407c3f622e07b7a71ec4b29a95927d6bb34
Owen Prosser <owenprosser@icloud.com>
2023-08-12 09:03:37 +0100

    Update env.mjs

--- env.mjs
+++ env.mjs
@@ -5,11 +5,11 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string(),
     DRIZZLE_DATABASE_URL: z.string(),
-    NEXTAUTH_URL: z.string().url().optional(),
-    NEXTAUTH_SECRET: z.string().min(1),
     WEATHER_API_KEY: z.string().min(1),
+    UPLOADTHING_SECRET: z.string().min(1),
+    UPLOADTHING_APP_ID: z.string().min(1),
     EMAIL_KEY: z.string().min(1),
-    EMAIL_FROM: z.string().min(1),
     EMAIL_FROM_NAME: z.string().min(1),
+    EMAIL_FROM: z.string().min(1),
   },
 });
