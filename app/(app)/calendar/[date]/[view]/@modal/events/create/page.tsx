b2afff5e2ddda6942a33d81c77451c42bb957405
Owen Prosser <owenprosser@icloud.com>
2023-08-19 20:40:48 +0100

    Remove labels

--- app/(app)/calendar/[date]/[view]/@modal/events/create/page.tsx
+++ app/(app)/calendar/[date]/[view]/@modal/events/create/page.tsx
@@ -72,61 +72,51 @@ const CreateEvent = async () => {
       <form action={action} className="space-y-6">
         <input type="hidden" name="userId" value={user.id} />
 
-        <div className="space-y-2">
-          {/* <Label htmlFor="title">Title</Label> */}
-          <Input
-            name="title"
-            id="title"
-            type="text"
-            required
-            minLength={3}
-            autoComplete="off"
-            autoCapitalize="off"
-            autoFocus
-            placeholder="Title"
-          />
-        </div>
+        <Input
+          name="title"
+          id="title"
+          type="text"
+          required
+          minLength={3}
+          autoComplete="off"
+          autoCapitalize="off"
+          autoFocus
+          placeholder="Title"
+        />
 
-        <div className="space-y-2">
-          <Label htmlFor="date">Date</Label>
-          <Input icon={Calendar} name="date" id="date" type="date" required />
-        </div>
+        <Input icon={Calendar} name="date" id="date" type="date" required />
         <Time />
 
-        <div className="space-y-2">
-          <Label htmlFor="description">Description</Label>
-          <Textarea name="description" id="description" className="h-24" />
-        </div>
+        <Textarea
+          name="description"
+          id="description"
+          placeholder="Description"
+          className="h-24"
+        />
 
-        <div className="space-y-2">
-          <Label htmlFor="subject">Subject</Label>
-          <Select
-            name="subjectId"
-            id="subject"
-            options={{
-              DEFAULT: "Select subject",
-              maths: "Maths",
-              chemistry: "Chemistry",
-              physics: "Physics",
-            }}
-          />
-        </div>
+        <Select
+          name="subjectId"
+          id="subject"
+          options={{
+            DEFAULT: "Select subject",
+            maths: "Maths",
+            chemistry: "Chemistry",
+            physics: "Physics",
+          }}
+        />
 
-        <div className="space-y-2">
-          <Label htmlFor="category">Category</Label>
-          <Select
-            name="category"
-            id="category"
-            required
-            options={{
-              DEFAULT: "Select category",
-              Test: "Test",
-              Meeting: "Meeting",
-              School: "School",
-              Other: "Other",
-            }}
-          />
-        </div>
+        <Select
+          name="category"
+          id="category"
+          required
+          options={{
+            DEFAULT: "Select category",
+            Test: "Test",
+            Meeting: "Meeting",
+            School: "School",
+            Other: "Other",
+          }}
+        />
 
         <FormButton>Add event</FormButton>
       </form>
