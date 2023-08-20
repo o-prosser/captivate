7c0d3259d1fd6ac55ad8dad366b0d6b941cb0850
Owen Prosser <owenprosser@icloud.com>
2023-08-20 19:27:07 +0100

    Even out sidebar margins

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -25,7 +25,7 @@ const AppLayout = async ({ children }: { children: React.ReactNode }) => {
           px-6 pt-[5.5rem] pb-[7.5rem] below-md:!ml-0 min-h-screen
           md:mr-6 md:my-6 md:pt-2 md:pb-4 md:min-h-[calc(100vh-3rem)] md:border border-border/50 md:rounded-2xl
           print:!m-0 print:!px-8 print:!pt-16 print:border-none
-          group-data-[expanded=true]/expanded:ml-[15rem] ml-[15rem] group-data-[expanded=false]/expanded:ml-[5.5rem] transition-all duration-100
+          group-data-[expanded=true]/expanded:ml-60 ml-60 group-data-[expanded=false]/expanded:ml-[4.65625rem] transition-all duration-100
         "
         >
           <Header />
